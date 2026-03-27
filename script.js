849d6e7294b7dbefb6ff2b0a1d0906efc42d65f8
CTRL PLAY <ctrlplay.pc@gmail.com>
2026-03-27 13:13:25 -0300

    novo

--- script.js
+++ script.js
@@ -71,7 +71,10 @@ async function buscar() {
     let d = desafios[atual];
 
     let device = await navigator.bluetooth.requestDevice({
-      acceptAllDevices: true,
+      filters: [
+        { namePrefix: "BBC micro:bit" },
+        { namePrefix: "micro:bit" }
+      ],
       optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']
     });
 
